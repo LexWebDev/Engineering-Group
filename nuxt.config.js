e6f607b6868730f1f5263db58aa18cba63d68b87
LexGrozniy <alex.millern@gmail.com>
2020-10-10 13:02:58 +0300

    Добавление глобального файла scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,13 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@/assets/scss/main.scss'
   ],
+  styleResources: {
+    scss: [
+      '@/assets/scss/globals.scss' // путь к вашему файлу
+    ]
+  },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
@@ -34,7 +40,8 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
